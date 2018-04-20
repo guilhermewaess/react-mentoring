06e8ada4fb8318dcb1d721d427de2344322e7e79
Guilherme Waess <guilherme.waess@gmail.com>
2018-04-20 15:09:50 +0200

    sandbox files

---
+++ task1/sandbox/createElement.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+export default React.createElement(
+  "h1",
+  { mentorName: "Denis" },
+  `Hello, this is createElement component!`
+);
---
+++ task1/sandbox/functionalComponent.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default ({ mentorName }) => (
+  <h1>Hello {mentorName}, this is functional component!</h1>
+);
