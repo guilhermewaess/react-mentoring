112ba6f182c9f90b87c3877573151033142415f5
--global <guilherme_waess@epam.com>
2018-04-27 15:39:39 +0200

    Get starting with router

--- netflixroulette/src/App.tsx
+++ netflixroulette/src/App.tsx
@@ -1,20 +1,31 @@
 import * as React from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+} from 'react-router-dom';
 import './App.scss';
 
 import logo from './logo.svg';
 
+const NewHeader = () => (<header className="App-header">
+  <img src={logo} className="App-logo" alt="logo" />
+  <h1 className="App-title">Welcome to React</h1>
+</header>)
+
+const AppIntro = () => (<p className="App-intro">
+  To get started, edit <code>src/App.tsx</code> and save to reload.
+</p>)
+
 class App extends React.Component {
   public render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.tsx</code> and save to reload.
-        </p>
-      </div>
+      <Router>
+        <div className="App">
+          <NewHeader />
+          <Route exact={true} path='/' component={AppIntro} />
+          <Route path='/teste' component={NewHeader} />
+        </div>
+      </Router>
     );
   }
 }
