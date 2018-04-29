5593971158478cd796b5f1d9bdec385d4e753d94
Guilherme Waess <guilherme.waess@gmail.com>
2018-04-29 23:05:44 +0200

    Start with movie card

--- netflixroulette/src/App.tsx
+++ netflixroulette/src/App.tsx
@@ -1,24 +1,26 @@
 import * as React from 'react';
-import {
-  BrowserRouter as Router,
-  Redirect,
-  Route,
-} from 'react-router-dom';
+import { BrowserRouter as Router, Redirect, Route } from 'react-router-dom';
+import { Container } from 'reactstrap';
 import './App.scss';
 
+import ErrorBoundary from './core/ErrorBoundary';
 import MoviesView from './views/MoviesView';
 
-const RootRedirect = () => <Redirect to='/movies' />;
+const RootRedirect = () => <Redirect to="/movies" />;
 
 class App extends React.Component {
   public render() {
     return (
-      <Router>
-        <div className="app">
-          <Route exact={true} path='/' component={RootRedirect} />
-          <Route path="/movies" component={MoviesView} />
-        </div>
-      </Router>
+      <Container fluid={true}>
+        <ErrorBoundary>
+          <Router>
+            <div className="app">
+              <Route exact={true} path="/" component={RootRedirect} />
+              <Route path="/movies" component={MoviesView} />
+            </div>
+          </Router>
+        </ErrorBoundary>
+      </Container>
     );
   }
 }
