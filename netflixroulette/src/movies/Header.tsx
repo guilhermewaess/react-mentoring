af407a8c086fe1a890155e702896dd61972f59e5
Guilherme Waess <guilherme.waess@gmail.com>
2018-05-01 16:49:07 +0200

    styles on header

--- netflixroulette/src/movies/Header.tsx
+++ netflixroulette/src/movies/Header.tsx
@@ -1,15 +1,30 @@
 import * as React from 'react';
+import { Col, Container, Row } from 'reactstrap';
 import './Header.scss';
 
 export class Header extends React.Component {
   public render() {
     return (
-      <div className=' movies-header'>
-        <div>Netflixroulette</div>
-        <div className="d-flex flex-column justify-content-center">
-          <div className='align-self-center'>SIR</div>
-        </div>
-      </div>
+      <Container fluid={true} className="movies-header">
+        <div className="text-white-50">Netflixroulette</div>
+        <Row className="h-100">
+          <Col
+            xs={{ offset: 2, size: 8 }}
+            className="align-self-center text-white"
+          >
+            <label className="mb-1 font-weight-bold">Find your movie</label>
+            <div className="shadow-lg search-input">
+              <input
+                className="w-100
+                           border-0
+                           rounded"
+                type="text"
+                placeholder="Search your movie"
+              />
+            </div>
+          </Col>
+        </Row>
+      </Container>
     );
   }
 }
