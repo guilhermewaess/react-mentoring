85ebe4146a891c6252fe75074a1ea4e58c07331f
Guilherme Waess <guilherme.waess@gmail.com>
2018-05-01 23:41:35 +0200

    Movies as props more components

--- netflixroulette/src/containers/Movies.tsx
+++ netflixroulette/src/containers/Movies.tsx
@@ -4,14 +4,90 @@ import { FilterResults } from './../movies/FilterResults';
 import { Header } from './../movies/Header';
 import { MoviesList } from './../movies/MoviesList';
 
-export default () => {
-  return (
-    <div className='w-100 h-100'>
-      <Header />
-      <FilterResults />
-      <Container fluid={true}>
-        <MoviesList />
-      </Container>
-    </div>
-  );
-};
+interface IState {
+  movies: any[];
+}
+
+export default class MoviesContainer extends React.Component {
+  public state: IState;
+
+  constructor(props: any) {
+    super(props);
+    this.state = {
+      movies: [
+        {
+          budget: 0,
+          genres: ['Action', 'Adventure', 'Science Fiction'],
+          id: 447365,
+          overview: "The third film based on Marvel's Guardians of the Galaxy.",
+          poster_path:
+            'https://image.tmdb.org/t/p/w500/ldoY4fTZkGISMidNw60GHoNdgP8.jpg',
+          release_date: '2020-05-01',
+          revenue: 0,
+          runtime: null,
+          tagline: '',
+          title: 'Guardians of the Galaxy Vol. 3',
+          vote_average: 0,
+          vote_count: 9,
+        },
+        {
+          budget: 0,
+          genres: ['Action', 'Adventure', 'Science Fiction'],
+          id: 447366,
+          overview: "The third film based on Marvel's Guardians of the Galaxy.",
+          poster_path:
+            'https://image.tmdb.org/t/p/w500/ldoY4fTZkGISMidNw60GHoNdgP8.jpg',
+          release_date: '2020-05-01',
+          revenue: 0,
+          runtime: null,
+          tagline: '',
+          title: 'Guardians of the Galaxy Vol. 3',
+          vote_average: 0,
+          vote_count: 9,
+        },
+        {
+          budget: 0,
+          genres: ['Action', 'Adventure', 'Science Fiction'],
+          id: 447367,
+          overview: "The third film based on Marvel's Guardians of the Galaxy.",
+          poster_path:
+            'https://image.tmdb.org/t/p/w500/ldoY4fTZkGISMidNw60GHoNdgP8.jpg',
+          release_date: '2020-05-01',
+          revenue: 0,
+          runtime: null,
+          tagline: '',
+          title: 'Guardians of the Galaxy Vol. 3',
+          vote_average: 0,
+          vote_count: 9,
+        },
+        {
+          budget: 0,
+          genres: ['Action', 'Adventure', 'Science Fiction'],
+          id: 447368,
+          overview: "The third film based on Marvel's Guardians of the Galaxy.",
+          poster_path:
+            'https://image.tmdb.org/t/p/w500/ldoY4fTZkGISMidNw60GHoNdgP8.jpg',
+          release_date: '2020-05-01',
+          revenue: 0,
+          runtime: null,
+          tagline: '',
+          title: 'Guardians of the Galaxy Vol. 3',
+          vote_average: 0,
+          vote_count: 9,
+        },
+      ],
+    };
+  }
+
+  public render() {
+    return (
+      <div className="w-100 h-100">
+        <Header />
+        <FilterResults movies={this.state.movies} />
+        <Container fluid={true}>
+          <MoviesList movies={this.state.movies} />
+        </Container>
+      </div>
+    );
+  }
+}
