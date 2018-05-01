e40b555a3c3b0c04ed73a032b36c6f9ea6a6fea2
Guilherme Waess <guilherme.waess@gmail.com>
2018-05-01 16:59:57 +0200

    fix app position extract header component

--- netflixroulette/src/movies/Header.tsx
+++ netflixroulette/src/movies/Header.tsx
@@ -2,6 +2,21 @@ import * as React from 'react';
 import { Col, Container, Row } from 'reactstrap';
 import './Header.scss';
 
+const SearchInput = () => (
+  <div>
+    <label className="mb-1 font-weight-bold">Find your movie</label>
+    <div className="shadow-lg search-input">
+      <input
+        className="w-100
+                border-0
+                rounded"
+        type="text"
+        placeholder="Search your movie"
+      />
+    </div>
+  </div>
+);
+
 export class Header extends React.Component {
   public render() {
     return (
@@ -12,16 +27,7 @@ export class Header extends React.Component {
             xs={{ offset: 2, size: 8 }}
             className="align-self-center text-white"
           >
-            <label className="mb-1 font-weight-bold">Find your movie</label>
-            <div className="shadow-lg search-input">
-              <input
-                className="w-100
-                           border-0
-                           rounded"
-                type="text"
-                placeholder="Search your movie"
-              />
-            </div>
+            <SearchInput />
           </Col>
         </Row>
       </Container>
